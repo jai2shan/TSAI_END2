ff6d629c93bbbc79afcd2f028665df40726965a5
jai2shan <muralis2raj@gmail.com>
2021-06-24 19:56:28 +0530

    updating

--- Session 5/dataprep.py
+++ Session 5/dataprep.py
@@ -1,8 +1,4 @@
-import pandas as pd
-import os
-os.chdir(r'/mnt/567ABBD17ABBABDD/Learning/TSAI/END_2_Assignments/Session 5/stanfordSentimentTreebank')
 
-sents = pd.read_csv(r'datasetSentences.txt', sep = '\t')
-p_dct = pd.read_csv(r'dictionary.txt',sep = '|', header = None, names=['phrase','phrase ids'])
-slbls = pd.read_csv(r'sentiment_labels.txt', sep = "|")
-dsplt = pd.read_csv(r'datasetSplit.txt', sep = ",")
+from itertools import combinations
+input_ = [-1,1,2,0,-1,-4]
+a = combinations(input_,3)
